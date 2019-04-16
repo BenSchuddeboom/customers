6c6746a7c85961aab03531b3d4cd74ca63afee15
BenSchuddeboom <Benzel@live.nl>
2019-04-16 17:54:24 +0200

    fixed config vars

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Sequelize = require('sequelize')
 const bodyParser = require('body-parser')
 
-const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.DATABASE_URL || 'postgres://pmukhpuitogbmr:64922121e70483c6dd2536aa6484adc5b06a73c1adbfa921ecdd1ba047ee872c@ec2-54-83-205-27.compute-1.amazonaws.com:5432/dej5o75vl0mjvr'
 const sequelize = new Sequelize(connectionString, {desfine: { timestamps: false }})
 const app = express()
 
